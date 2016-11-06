94ce9e403fea796a00089285e1dafaa13dfcb67f
Ozan KANIK <ozankanik@hotmail.com>
2016-11-06 14:15:23 +0300

    .Net Framework 2.0 to 3.5

--- Common/Helpers/Extensions.cs
+++ Common/Helpers/Extensions.cs
@@ -4,85 +4,77 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace System.Runtime.CompilerServices
+public static class Extensions
 {
-    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class
-         | AttributeTargets.Method)]
-    public sealed class ExtensionAttribute : Attribute { }
-
-    public static class Extensions
+    public static void Shuffle<T>(this IList<T> list)
     {
-        public static void Shuffle<T>(this IList<T> list)
+        RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+        int n = list.Count;
+        while (n > 1)
         {
-            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
-            int n = list.Count;
-            while (n > 1)
-            {
-                byte[] box = new byte[1];
-                do provider.GetBytes(box);
-                while (!(box[0] < n * (System.Byte.MaxValue / n)));
-                int k = (box[0] % n);
-                n--;
-                T value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
-
-        public static bool Contains(this IList<Common.Card> list, CardType cardType, CardValue cardValue)
-        {
-            foreach (Card card in list)
-            {
-                if (card.GetCardValue() == cardValue && card.GetCardType() == cardType)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public static Card GetCard(this IList<Common.Card> list, CardType cardType, CardValue cardValue)
-        {
-            foreach (Card card in list)
-            {
-                if (card.GetCardValue() == cardValue && card.GetCardType() == cardType)
-                {
-                    return card;
-                }
-            }
-
-            return null;
-        }
-
-
-        public static Card GetSomeCardBiggerThan(this IList<Common.Card> list, Card card)
-        {
-            int i = 0;
-            while (i < list.Count)
-            {
-                if (list[i].GetCardType() == card.GetCardType() && list[i].GetNumericValue() >= card.GetNumericValue())
-                {
-                    return list[i];
-                }
-                i++;
-            }
-            return null;
-        }
-
-        public static Card GetCardOfType(this IList<Common.Card> list, CardType type)
-        {
-            int i = 0;
-            while (i < list.Count)
-            {
-                if (list[i].GetCardType() == type)
-                {
-                    return list[i];
-                }
-                i++;
-            }
-            return null;
+            byte[] box = new byte[1];
+            do provider.GetBytes(box);
+            while (!(box[0] < n * (System.Byte.MaxValue / n)));
+            int k = (box[0] % n);
+            n--;
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
         }
     }
 
+    public static bool Contains(this IList<Common.Card> list, CardType cardType, CardValue cardValue)
+    {
+        foreach (Card card in list)
+        {
+            if (card.GetCardValue() == cardValue && card.GetCardType() == cardType)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static Card GetCard(this IList<Common.Card> list, CardType cardType, CardValue cardValue)
+    {
+        foreach (Card card in list)
+        {
+            if (card.GetCardValue() == cardValue && card.GetCardType() == cardType)
+            {
+                return card;
+            }
+        }
+
+        return null;
+    }
+
+
+    public static Card GetSomeCardBiggerThan(this IList<Common.Card> list, Card card)
+    {
+        int i = 0;
+        while (i < list.Count)
+        {
+            if (list[i].GetCardType() == card.GetCardType() && list[i].GetNumericValue() >= card.GetNumericValue())
+            {
+                return list[i];
+            }
+            i++;
+        }
+        return null;
+    }
+
+    public static Card GetCardOfType(this IList<Common.Card> list, CardType type)
+    {
+        int i = 0;
+        while (i < list.Count)
+        {
+            if (list[i].GetCardType() == type)
+            {
+                return list[i];
+            }
+            i++;
+        }
+        return null;
+    }
 }
